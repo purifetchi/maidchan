919b46b6f8c3301a8b6e9c845b6edc0b6eb97f16
naomiEve <stapa2000@gmail.com>
2017-10-10 21:59:09 +0200

    rough moderation login sketch

--- app.rb
+++ app.rb
@@ -5,13 +5,27 @@ require 'digest'
 
 db = SQLite3::Database.new "maidchan.db"
 
+# Configuration (Janitors, etc...)
+config_raw = File.read('./config.json')
+config = JSON.parse(config_raw)
+
+enable :sessions
+
+set :config, config
 set :public_folder, File.dirname(__FILE__) + '/static'
 set :port, 80
 set :bind, '0.0.0.0'
 
 # Helper functions
-
 types = ['image/png', 'image/jpeg', 'image/gif']
+
+def get_ip(req, env) #Gets the ip of the user
+	ip = req.ip
+	if ip == "127.0.0.1"
+		ip = env["HTTP_X_FORWARDED_FOR"]
+	end
+	return ip
+end
 
 def tripcode!(trip) #Generates a hash out of a tripcode
 	return Digest::SHA256.base64digest(trip)[0..10]
@@ -28,6 +42,10 @@ def store!(params) #Stores the file and returns the filename
 	return filename
 end
 
+def is_moderator?(session)
+	return !session[:moderator].nil?
+end
+
 # GET requests, A.K.A. regular routes
 
 get '/' do
@@ -35,7 +53,16 @@ get '/' do
 end
 
 get '/topic/:id' do |id|
-	erb :topic, :locals => {:db => db, :id => id.to_i}
+	erb :topic, :locals => {:db => db, :id => id.to_i, :session => session}
+end
+
+get '/mod' do
+	erb :login
+end
+
+get '/logout' do
+	session[:moderator] = nil
+	redirect '/mod', 303
 end
 
 # POST requests, A.K.A. functional routes
@@ -55,9 +82,9 @@ post '/post' do
 
 	if params[:file] and types.include?(params[:file][:type]) and params[:file][:tempfile].size < 3145728
 		filename = store!(params)
-		db.execute("INSERT INTO posts (title, comment, author, image) VALUES (?, ?, ?, ?)", params[:title], params[:comment], name, filename)
+		db.execute("INSERT INTO posts (title, comment, author, image, ip) VALUES (?, ?, ?, ?, ?)", params[:title], params[:comment], name, filename, get_ip(request, env))
 	else
-		db.execute("INSERT INTO posts (title, comment, author) VALUES (?, ?, ?)", params[:title], params[:comment], name)
+		db.execute("INSERT INTO posts (title, comment, author, ip) VALUES (?, ?, ?, ?)", params[:title], params[:comment], name, get_ip(request, env))
 	end	
 	redirect '/', 303
 end
@@ -79,9 +106,18 @@ post '/reply/:id' do |id|
 
 	if params[:file] and types.include?(params[:file][:type]) and params[:file][:tempfile].size < 3145728
 		filename = store!(params)
-		db.execute("INSERT INTO posts (parent, comment, author, image) VALUES (?, ?, ?, ?)", id.to_i, params[:comment], name, filename)
+		db.execute("INSERT INTO posts (parent, comment, author, image, ip) VALUES (?, ?, ?, ?, ?)", id.to_i, params[:comment], name, filename, get_ip(request, env))
 	else
-		db.execute("INSERT INTO posts (parent, comment, author) VALUES (?, ?, ?)", id.to_i, params[:comment], name)
+		db.execute("INSERT INTO posts (parent, comment, author, ip) VALUES (?, ?, ?, ?)", id.to_i, params[:comment], name, get_ip(request, env))
 	end
 	redirect '/topic/' + id, 303
+end
+
+post '/mod' do
+	if config["janitors"][params[:name]] and config["janitors"][params[:name]]["pass"] == params[:pass]
+		session[:moderator] = params[:name]
+		redirect '/', 303
+	else
+		return[401, "Invalid username or password."]
+	end
 end
